6d332554635370b88da93b4948c0ef4bd19ea24a
Amy F Schaeffer <65150396+afschaeffer@users.noreply.github.com>
2021-04-20 11:55:27 -0700

    Add files via upload

---
+++ Assignment 6.1.py
@@ -0,0 +1,21 @@
+# Assignment 6, Amy Schaeffer
+print("Welcome to the miles to kilometers calculator")
+def m_to_km(miles_inputted):
+            """Converts entered miles to kilometers"""
+            kilometers = miles_inputted * 1.609
+            print(f"{miles_inputted} miles is equal to {kilometers} kilometers.")
+while True:
+    while True:
+        miles = input('How many miles did you drive? : ')
+        try:
+            value = float(miles)
+            print("Thank you for your milage input of ", value)
+            m_to_km(value)
+            break
+        except ValueError:
+            print("Input invalid, please enter a number.")
+    stay = input('Type "q" to quit, or type anything to return to prompt : ' )
+    if stay == "q":
+        break
+    
+    
